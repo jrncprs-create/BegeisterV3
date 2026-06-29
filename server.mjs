f2a0dd182908c47844b3f66c2d0a8610bf7fc7ff
jrncprs-create <jrncprs@gmail.com>
2026-06-29 17:57:59 +0200

    Add files via upload

--- server.mjs
+++ server.mjs
@@ -40,6 +40,7 @@ app.use("/api/opdracht", express.raw({
 await mount("/api/opdracht", "./api/opdracht.mjs");
 await mount("/api/readdrop", "./api/readdrop.mjs");
 await mount("/api/wa-subscribe", "./api/wa-subscribe.mjs");
+await mount("/api/projtodos", "./api/projtodos.mjs");
 await mount("/api/chat", "./api/chat.mjs");
 await mount("/api/vision", "./api/vision.mjs");
 await mount("/api/readfile", "./api/readfile.mjs");
