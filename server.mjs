b77e76b85301de2888e10fba978e2a3c5915fef2
jrncprs-create <jrncprs@gmail.com>
2026-07-10 16:16:54 +0200

    server: weer-route

--- server.mjs
+++ server.mjs
@@ -56,6 +56,7 @@ await mount("/api/sortfiles", "./api/sortfiles.mjs");
 await mount("/api/triage", "./api/triage.mjs");
 await mount("/api/docxview", "./api/docxview.mjs");
 await mount("/api/snelkoppeling", "./api/snelkoppeling.mjs");
+await mount("/api/weer", "./api/weer.mjs");
 await mount("/api/fileproxy", "./api/fileproxy.mjs");
 await mount("/api/notify", "./api/notify.mjs");
 await mount("/api/push", "./api/push.mjs");
